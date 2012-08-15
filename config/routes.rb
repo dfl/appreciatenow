359cfc2459d4c7faa92f5c947e1abb3cf104920f
David Lowenfels <david@internautdesign.com>
2012-08-14 18:16:25 -0700

    admin shortcut

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ AppreciateNow::Application.routes.draw do
   resources :approvals, :only => [:index, :update, :destroy]
    
   match "random", :to => "appreciations#random"
+  match "admin", :to => "approvals#index"
   match ":id", :to => "appreciations#show", :constraints => {:id => /\d+/}
 
   root :to => 'appreciations#random'
